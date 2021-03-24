ec355a11bb2fb8a91cdbd06a6053dc323a724048
Luis Montoya <luis.e.montoya@exatec.tec.mx>
2021-03-23 18:04:52 -0600

    [FIX] Dependencies

--- mayavi/mayavi_install.sh
+++ mayavi/mayavi_install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 # Install Mayavi (scientific data visualization and plotting in Python) on Ubuntu
 # Ref: http://docs.enthought.com/mayavi/mayavi/installation.html
-apt-get install python-vtk python-qt4 python-qt4-gl python-setuptools python-numpy python-configobj
-pip install mayavi
+apt-get install python-vtk python-qt4 python-qt4-gl python-setuptools python-numpy python-configobj libgtk-3-dev
+pip3 install mayavi wxpython
